422c021e8ef2e6d50577d22ba94f3d14935601c0
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-01-12 21:53:20 +0900

    correct: 'MyUserManger' class name to 'UserManager'

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
-class MyUserManger(BaseUserManager):
+class UserManager(BaseUserManager):
     def create_user(self, email, username, password=None):
         if not email:
             raise ValueError("Users must have an email address.")
