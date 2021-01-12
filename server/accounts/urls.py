5daff42c84060e3e4e0f8df98a865b100ddab395
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-01-12 22:23:03 +0900

    remove: unnessary comments

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,10 +1,3 @@
-# from .views import sign_up
-# from django.urls import path
-#
-# urlpatterns = [
-#     path('api/signup', sign_up),
-# ]
-
 from rest_framework import routers
 from . import views
 from django.urls import path, include
