d6fe258c2679d825f6bf1d2cc2ad213ee8d228e1
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-01-21 22:59:42 +0900

    add: connect mariadb by using docker

--- server/todo/settings.py
+++ server/todo/settings.py
@@ -67,9 +67,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'todo',
-        'USER': 'root',
-        'PASSWORD': '8867',
-        'HOST': 'localhost',
+        'USER': 'ahu8867',
+        'PASSWORD': 'ahu8867pw',
+        'HOST': '127.0.0.1',
         'PORT': '3306'
     }
 }
