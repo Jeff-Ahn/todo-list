00377ce274a37609c394d1a057ff6cd89e5485d4
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-02-22 23:44:08 +0900

    add: todo model

--- server/todo/models.py
+++ server/todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+class Todo(models.Model):
+    content = models.TextField()
+    is_done = models.BooleanField(default=False)
+    owner = models.ForeignKey(
+        User, related_name="todos",
+        on_delete=models.CASCADE, null=True
+    )
+
+    def __str__(self):
+        return self.content
--- server/todo/views.py
+++ server/todo/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .serializers import TodoSerializers
 
-# Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = TodoSerializers
+
+    def get_queryset(self):
+        return self.request.user.todos.all()
+
