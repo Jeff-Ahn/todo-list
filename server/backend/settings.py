f0965d28b98269237e4bd4dfe8212eab8c547ac4
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-02-24 00:32:16 +0900

    fix: post todo when user == owner

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -12,7 +12,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -59,7 +58,6 @@ AUTH_USER_MODEL = "account.User"
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -70,29 +68,32 @@ DATABASES = {
         "USER": "ahu8867",
         "PASSWORD": "ahu8867pw",
         "HOST": "127.0.0.1",
+        # "HOST": "mariadb",
         "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME":
+        "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME":
+        "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME":
+        "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME":
+        "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
@@ -106,7 +107,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
--- server/todo/views.py
+++ server/todo/views.py
@@ -1,7 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
+
 from .serializers import TodoSerializers
 from .permissions import IsOwner
+from account.models import User
 
 
 class TodoViewSet(viewsets.ModelViewSet):
@@ -10,3 +14,14 @@ class TodoViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.request.user.todos.all()
+
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        data = request.data
+        ownerId = data['owner']
+        owner = User.objects.get(id=ownerId)
+
+        if owner != user:
+            raise PermissionDenied('다른 사용자의 todo를 생성할 수 없습니다.')
+
+        return super().create(request, args, kwargs)
