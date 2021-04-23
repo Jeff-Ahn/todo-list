c3794d875816c008797b337bceefda5e8f9694f0
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-04-23 19:59:52 +0900

    common api test 함수 생성(person 생성, 로그인한 person 생성)

---
+++ server/common/test.py
@@ -0,0 +1,17 @@
+from faker import Faker
+from server.account.models import User, Person
+
+fake = Faker()
+
+
+def create_person() -> Person:
+
+    user = User.objects.create(email=fake.safe_email(),
+                               password=fake.password())
+    person = Person.objects.create(
+        user=user,
+        first_name=fake.first_name(),
+        last_name=fake.last_name(),
+    )
+
+    return person
