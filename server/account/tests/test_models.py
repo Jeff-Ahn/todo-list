8cbdace50971a196f8dbccee786f03d3a0fe65fa
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-04-23 20:04:51 +0900

    비밀번호 변경 테스트 함수 추가

--- server/account/tests/test_models.py
+++ server/account/tests/test_models.py
@@ -1,13 +1,12 @@
 from rest_framework import status
 from rest_framework.reverse import reverse
-from rest_framework.test import APITestCase
-
+from server.common.api_test import CommonAPITestCase
 from faker import Faker
 
 fake = Faker()
 
 
-class AccountAPITest(APITestCase):
+class AccountAPITest(CommonAPITestCase):
     def test_create_account(self):
         signup_data = self._generate_valid_signup_data()
         response = self.client.post(reverse('account-list'),
@@ -28,12 +27,21 @@ class AccountAPITest(APITestCase):
     def test_login_with_invalid_id_password(self):
         signup_data = self._generate_valid_signup_data()
         self.client.post(reverse('account-list'), signup_data, format='json')
-
         response = self.client.post(reverse('login'), {
             "email": signup_data["email"],
             "password": "WRONG_PASSWORD"
         })
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_change_password(self):
+        person = self.create_person_with_login()
+        password = person.user.password
+        response = self.client.patch(reverse('account-change-password'),
+                                     {"password": "TEST_PASSWORD"},
+                                     format='json')
+        new_password = person.user.password
+        self.assertNotEqual(password, new_password)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def _generate_valid_signup_data(self):
         return {
