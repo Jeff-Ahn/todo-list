8c6c34f891055dadbc9bfefacef422e44190aea3
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-05-07 02:14:39 +0900

    todo CRUD 수정 with SRP 원칙

--- server/todo/tests.py
+++ server/todo/tests.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from server.common.api_test import CommonAPITestCase
+from server.todo.models import Todo
 from rest_framework.reverse import reverse
 from faker import Faker
 import json
@@ -16,48 +17,37 @@ class TodoAPITest(CommonAPITestCase):
                                          format='json')
 
     def test__create_todo__when__valid_request__expect__201_created(self):
-        new_todo = self._generate_valid_random_todo()
-        response = self.client.post(reverse('todo-list'),
-                                    new_todo,
-                                    format='json')
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Todo.objects.count(), 1)
 
     def test__get_todo__when__valid_request__expect__200_ok(self):
         response = self.client.get(reverse('todo-list'))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
 
     def test__update_todo_when__valid_request__expect__200_ok(self):
-        new_todo = self._generate_valid_random_todo()
-        response = self.client.post(reverse('todo-list'),
-                                    new_todo,
-                                    format='json')
-
-        current_todo = response.data
+        todo = Todo.objects.get()
         response = self.client.put(reverse('todo-detail',
-                                           kwargs={'pk': current_todo['id']}),
+                                           kwargs={'pk': todo.id}),
                                    data={
                                        "content": "NEW_CONTENT",
                                        "is_done": True
                                    },
                                    format='json')
-        current_todo = json.dumps(current_todo)
-        updated_todo = json.dumps(response.data)
+        new_todo = response.data
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertJSONNotEqual(current_todo, updated_todo)
+        self.assertNotEqual(todo.content, new_todo['content'])
+        self.assertNotEqual(todo.is_done, new_todo['is_done'])
 
     def test__delete_todo_when__valid_request__expect__204_no_content(self):
-        new_todo = self._generate_valid_random_todo()
-        response = self.client.post(reverse('todo-list'),
-                                    new_todo,
-                                    format='json')
-        current_todo = response.data
+        todo = Todo.objects.get()
         response = self.client.delete(
-            reverse('todo-detail', kwargs={'pk': current_todo['id']}))
+            reverse('todo-detail', kwargs={'pk': todo.id}))
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Todo.objects.count(), 0)
 
     def _generate_valid_random_todo(self):
         return {"content": fake.paragraph(nb_sentences=5), "is_done": False}
